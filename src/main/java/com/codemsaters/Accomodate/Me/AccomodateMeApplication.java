c52bff713b9c83902190a8586b185c0b854f47a9
Neo Boledi <neoboledi21@gmail.com>
2024-02-28 15:50:28 +0200

    first commit

---
+++ src/main/java/com/codemsaters/Accomodate/Me/AccomodateMeApplication.java
@@ -0,0 +1,13 @@
+package com.codemsaters.Accomodate.Me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccomodateMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccomodateMeApplication.class, args);
+	}
+
+}
